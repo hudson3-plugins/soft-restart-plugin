0d9a80250e1dbc0cc0c69924d1e61ff4a180081e
Bob Foster <bobfoster@gmail.com>
2013-07-03 17:21:01 -0700

    Renamed to org.hudsonci.lifecycle.SoftRestart per Winston's suggestion

--- src/main/java/org/hudsonci/lifecycle/SoftRestart.java
+++ src/main/java/org/hudsonci/lifecycle/SoftRestart.java
@@ -9,26 +9,30 @@
  *    Hudson - initial API and implementation and/or initial documentation
  */
 
-package org.hudsonci.plugins.restart.soft;
+package org.hudsonci.lifecycle;
 
 import hudson.lifecycle.Lifecycle;
 import hudson.lifecycle.RestartNotSupportedException;
 import hudson.model.Hudson;
 import java.io.IOException;
 import org.eclipse.hudson.init.InitialSetup;
+import org.hudsonci.plugins.restart.soft.Messages;
 
 /**
- *
+ * Soft restart attempts to restart Hudson within the same JVM.
+ * 
+ * Requires Hudson 3.1.0
+ * 
  * @author Bob Foster
  */
-public class SoftRestartLifecycle extends Lifecycle {
+public class SoftRestart extends Lifecycle {
 
     @Override
     public void restart() throws IOException, InterruptedException {
         if (!canRestart()) {
             throw new IOException(Messages.SoftRestartLifecycle_needSafeRestart());
         }
-        InitialSetup.getLastInitialSetup().invokeHudson();
+        InitialSetup.getLastInitialSetup().invokeHudson(true);
     }
 
     @Override
@@ -42,5 +46,10 @@ public class SoftRestartLifecycle extends Lifecycle {
     public boolean canRestart() {
         return Hudson.getInstance().isSafeRestarting();
     }
-
+    
+    /** SoftRestart is <i>only</i> safe restartable. */
+    @Override
+    public boolean isSafeRestartable() {
+        return true;
+    }
 }
